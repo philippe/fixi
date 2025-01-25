5a4f9d27752df63d15ecc10c4be110ab7a20c857
Carson Gross <carson@bigsky.software>
2025-01-25 16:17:07 -0700

    space savings

--- fixi.js
+++ fixi.js
@@ -1,12 +1,12 @@
 (()=>{
-	let send = (elt, type, detail, bub)=>elt.dispatchEvent(new CustomEvent("fx:" + type, {detail:detail, cancelable:true, bubbles:bub !== false, composed:true}))
-	let attr = (elt, attrName, defaultVal)=>elt.getAttribute(attrName) || defaultVal
+	let send = (elt, type, detail, bub)=>elt.dispatchEvent(new CustomEvent("fx:" + type, {detail, cancelable:true, bubbles:bub !== false, composed:true}))
+	let attr = (elt, name, defaultVal)=>elt.getAttribute(name) || defaultVal
 	let ignore = (elt)=>elt.matches("[fx-ignore]") || elt.closest("[fx-ignore]") != null
 	let init = (elt)=>{
 		let options = {}
 		if (elt.__fixi || ignore(elt) || !send(elt, "init", {options})) return
 		elt.__fixi = async(evt)=>{
-			let reqs = elt.__fixi.requests || (elt.__fixi.requests = [])
+			let reqs = elt.__fixi.requests ||= []
 			let targetSelector = attr(elt, "fx-target")
 			let target = targetSelector ? document.querySelector(targetSelector) : elt
 			let headers = {"FX-Request":"true"}
@@ -20,7 +20,7 @@
 			let drop = reqs.length > 0
 			let cfg = {trigger:evt, method, action, headers, target, swap, body, drop, signal:abort.signal, abort:(r)=>abort.abort(r), preventTriggerDefault:true, transition:true, fetch}
 			if (!send(elt, "config", {evt, cfg, requests:reqs}) || cfg.drop) return
-			if ((cfg.method === "GET" || cfg.method === "DELETE") && cfg.body){
+			if (/GET|DELETE/.test(cfg.method) && cfg.body){
 				if (!cfg.body.entries().next().done) cfg.action += (cfg.action.indexOf("?") > 0 ? "&" : "?") + new URLSearchParams(cfg.body).toString()
 				cfg.body = null
 			}
@@ -48,9 +48,9 @@
 					return cfg.swap(cfg)
 				} else if (/(before|after)(start|end)/.test(cfg.swap)){
 					cfg.target.insertAdjacentHTML(cfg.swap, cfg.text)
-				} else {
+				} else if(cfg.swap in cfg.target){
 					cfg.target[cfg.swap] = cfg.text
-				}
+				} else throw cfg.swap
 			}
 			if (cfg.transition && document.startViewTransition){
 				await document.startViewTransition(doSwap).finished
