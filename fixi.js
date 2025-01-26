4e961860d845656930477aee8a34af4eb97eca2d
Philippe Archambault <parchambault@cienapps.com>
2025-01-25 23:04:54 -0500

    add config options, inheritance of attributes,  target="this" and a logger

--- fixi.js
+++ fixi.js
@@ -1,37 +1,35 @@
-(()=>{
-	let send = (elt, type, detail, bub)=>elt.dispatchEvent(new CustomEvent("fx:" + type, {detail, cancelable:true, bubbles:bub !== false, composed:true}))
-	let attr = (elt, name, defaultVal)=>elt.getAttribute(name) || defaultVal
-	let ignore = (elt)=>elt.matches("[fx-ignore]") || elt.closest("[fx-ignore]") != null
+let fixi = { prefix:"fx-", headers:{"FX-Request":"true"}, default:{swap:"outerHTML"}, logger:()=>{}};
+((fx)=>{
+	let evtName = (evt)=> (fx.prefix ? fx.prefix.replace("-", ":"): "fixi:")+evt;
+	let send = (elt, type, detail, bub)=>{fx.logger(type=evtName(type), elt, detail); return elt.dispatchEvent(new CustomEvent(type, {detail:detail, cancelable:true, bubbles:bub !== false, composed:true}))}
+	let attr = (elt, attr, defaultVal=null)=>elt.closest("["+fx.prefix+attr+"]")?.getAttribute(fx.prefix+attr) || defaultVal
+	let ignore = (elt)=>attr(elt, "ignore", null) !== null
 	let init = (elt)=>{
 		let options = {}
 		if (elt.__fixi || ignore(elt) || !send(elt, "init", {options})) return
 		elt.__fixi = async(evt)=>{
 			let reqs = elt.__fixi.requests ||= new Set()
-			let targetSelector = attr(elt, "fx-target")
-			let target = targetSelector ? document.querySelector(targetSelector) : elt
-			let headers = {"FX-Request":"true"}
-			let method = attr(elt, "fx-method", "GET").toUpperCase()
-			let action = attr(elt, "fx-action", "")
-			let swap = attr(elt, "fx-swap", "outerHTML")
+			let targetSelector = attr(elt, "target")
+			let target = targetSelector === "this" ? elt.closest(`[${fx.prefix}target]`): document.querySelector(targetSelector) || elt
+			let method = attr(elt, "method", "GET").toUpperCase()
+			let action = attr(elt, "href", attr(elt, "action", ""))
+			let swap = attr(elt, "swap", fx.default.swap)
 			let form = elt.form || elt.closest("form")
 			let body = new FormData(form || undefined, evt.submitter)
 			if (!form && elt.name) body.append(elt.name, elt.value)
 			let abort = new AbortController()
 			let drop = reqs.length > 0
-			let cfg = {trigger:evt, method, action, headers, target, swap, body, drop, signal:abort.signal, abort:(r)=>abort.abort(r),
-				preventTriggerDefault:true, transition:document.startViewTransition?.bind(document), fetch:fetch.bind(window)}
+			let cfg = {trigger:evt, method, action, headers:fx.headers, target, swap, body, drop, signal:abort.signal, abort:(r)=>abort.abort(r),
+				preventDefault:true, transition:document.startViewTransition?.bind(document), fetch:fetch.bind(window)}
 			if (!send(elt, "config", {evt, cfg, requests:reqs}) || cfg.drop) return
-			if (/GET|DELETE/.test(cfg.method)){
-				if (!cfg.body.entries().next().done) cfg.action += (cfg.action.indexOf("?") > 0 ? "&" : "?") + new URLSearchParams(cfg.body).toString()
+			if (/GET|DELETE/.test(cfg.method)) {
+				if (!cfg.body.entries().next().done) cfg.action += (cfg.action.indexOf("?") > 0 ? "&": "?") + new URLSearchParams(cfg.body).toString()
 				cfg.body = null
 			}
-			if (cfg.preventTriggerDefault) evt.preventDefault()
+			if (cfg.preventDefault) evt.preventDefault()
 			reqs.add(cfg)
 			try {
-				if (cfg.confirm){
-					let result = await cfg.confirm()
-					if (!result) return
-				}
+				if (cfg.confirm && !await cfg.confirm()) return
 				if (!send(elt, "before", {evt, cfg, requests:reqs})) return
 				cfg.response = await cfg.fetch(cfg.action, cfg)
 				cfg.text = await cfg.response.text()
@@ -49,7 +47,7 @@
 					return cfg.swap(cfg)
 				else if (/(before|after)(start|end)/.test(cfg.swap))
 					cfg.target.insertAdjacentHTML(cfg.swap, cfg.text)
-				else if(cfg.swap in cfg.target)
+				else if (cfg.swap in cfg.target)
 					cfg.target[cfg.swap] = cfg.text
 				else throw cfg.swap
 			}
@@ -59,21 +57,21 @@
 				await doSwap()
 			send(elt, "swapped", {cfg})
 		}
-		elt.__fixi.evt = attr(elt, "fx-trigger", elt.matches("form") ? "submit" : elt.matches("input:not([type=button]),select,textarea") ? "change" : "click")
+		elt.__fixi.evt = attr(elt, "trigger", elt.matches("form") ? "submit" : elt.matches("input:not([type=button]),select,textarea") ? "change" : "click")
 		elt.addEventListener(elt.__fixi.evt, elt.__fixi, options)
 		send(elt, "inited", {}, false)
 	}
 	let process = (elt)=>{
 		if (elt instanceof Element){
 			if (ignore(elt)) return
-			if (elt.matches("[fx-action]")) init(elt)
-			elt.querySelectorAll("[fx-action]").forEach((elt)=>init(elt))
+			let attrs = `[${fx.prefix}href],[${fx.prefix}action]`
+			if (elt.matches(attrs)) init(elt)
+			elt.querySelectorAll(attrs).forEach((elt)=>init(elt))
 		}
 	}
-	document.addEventListener("fx:process", (evt)=>process(evt.target))
+	document.addEventListener(evtName("process"), (evt)=>process(evt.target))
 	document.addEventListener("DOMContentLoaded", ()=>{
-		document.__fixi_mo = new MutationObserver((recs)=>recs.forEach((r)=>r.type === "childList" && r.addedNodes.forEach((n)=>process(n))))
-		document.__fixi_mo.observe(document.body, {childList:true, subtree:true})
+		(fixi.mo = new MutationObserver((recs)=>recs.forEach((r)=>r.type === "childList" && r.addedNodes.forEach((n)=>process(n))))).observe(document.body, {childList:true, subtree:true})
 		process(document.body)
 	})
-})()
+})(fixi)
